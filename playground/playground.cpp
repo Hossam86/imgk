053a5c2e96d1daf0110f664c05ed980a8718e7f1
hossamali <hossamsamir@outlook.com>
2024-05-26 23:24:28 +0300

    convert image to gray scale

--- playground/playground.cpp
+++ playground/playground.cpp
@@ -3,14 +3,26 @@
 #include <imgk/Image.h>
 
 int main() {
-    Image img("C:\\projects\\imgk\\resources\\flower.jpg");
-    img.write("new.png");
-    Image copy(img);
-    for (int i = 0; i < copy.w * copy.channels; ++i) {
-        copy.data[i] = 0;
-    }
-    copy.write("copy.png");
-    Image blank(100, 100, 3);
-    blank.write("blank.jpg");
+    // test image loading, copy and write
+//    Image img("C:\\projects\\imgk\\resources\\flower.jpg");
+//    img.write("new.png");
+//    Image copy(img);
+//    for (int i = 0; i < copy.w * copy.channels; ++i) {
+//        copy.data[i] = 0;
+//    }
+//    copy.write("copy.png");
+//    Image blank(100, 100, 3);
+//    blank.write("blank.jpg");
 
+
+// test gray scale filter
+
+    Image test("C:\\projects\\imgk\\resources\\flower.jpg");
+    Image gray_avg = test;
+    gray_avg.grayscale_avg();
+    gray_avg.write("gray_avg.png");
+
+    Image gray_lum = test;
+    gray_lum.grayscale_lum();
+    gray_lum.write("gray_lum.png");
 }
