4fa206d45af62397e9cd1dc13092e8fd5b039662
hossamali <hossamsamir@outlook.com>
2024-05-26 20:41:24 +0300

    Image object

--- playground/playground.cpp
+++ playground/playground.cpp
@@ -1,6 +1,16 @@
 // Created by Hossam Samir on 05/09/2023.
 
-#include "imgk/Image.h"
+#include <imgk/Image.h>
+
 int main() {
+    Image img("C:\\projects\\imgk\\resources\\flower.jpg");
+    img.write("new.png");
+    Image copy(img);
+    for (int i = 0; i < copy.w * copy.channels; ++i) {
+        copy.data[i] = 0;
+    }
+    copy.write("copy.png");
+    Image blank(100, 100, 3);
+    blank.write("blank.jpg");
 
 }
